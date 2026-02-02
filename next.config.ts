c1424e2ec93bea0788a453ede408326ca512d81e
tigrankhh <khachtik2606@gmail.com>
2026-02-02 20:06:10 +0400

    Remove comments from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Включаем игнорирование ошибок при сборке
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Игнорируем ошибки линтера
     ignoreDuringBuilds: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
