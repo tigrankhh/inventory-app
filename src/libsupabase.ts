d04c9f6ab63277b99bb2ac239d5ba988dc52e981
tigrankhh <khachtik2606@gmail.com>
2026-02-03 07:09:59 +0400

    Update libsupabase.ts

--- src/libsupabase.ts
+++ src/libsupabase.ts
@@ -1,8 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-// Эти переменные Next.js берет из настроек Netlify (Environment Variables)
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-// Создаем один экземпляр клиента для всего приложения
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(url, key)
+export default supabase
