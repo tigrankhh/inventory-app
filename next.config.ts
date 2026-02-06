d297ea5348e903b648c47525bbab335e063ef190
tigrankhh <khachtik2606@gmail.com>
2026-02-06 07:53:59 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    // Если хочешь пропустить ошибки типов при билде (крайний случай)
+    // ignoreBuildErrors: true, 
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
-    unoptimized: true, // Это критично для Cloudflare Pages
+    unoptimized: true,
   },
 };
 
