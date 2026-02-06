52aa0dea700f3b2ac516e6d527f07fbfb3307e43
tigrankhh <khachtik2606@gmail.com>
2026-02-06 08:00:30 +0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Отключаем оптимизацию картинок, так как в Edge она работает иначе
-  images: {
-    unoptimized: true,
-  },
-  // Игнорируем ошибки линтинга и типов, чтобы билд прошел 100%
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true, 
-  },
+  images: { unoptimized: true },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
