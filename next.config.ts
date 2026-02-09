3d1a5622c7534691f6551c364843c10071530706
tigrankhh <khachtik2606@gmail.com>
2026-02-09 06:19:02 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // ⚠️ Важно: Cloudflare не поддерживает стандартную оптимизацию картинок Next.js без доп. настроек
-  images: {
-    unoptimized: true,
-  },
-  // Убедись, что нет лишних редиректов или basePath, которые могут давать 404
+  /* config options here */
 };
 
 export default nextConfig;
