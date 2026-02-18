937b9f3b09c5d9831267f869e27563a2873dd50e
tigrankhh <khachtik2606@gmail.com>
2026-02-18 19:42:56 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-export const runtime = 'edge';
+export const runtime = 'edge'; // Это должно быть в самом верху файла
 
 export default function Home() {
   return <h1 style={{color: 'white', padding: '50px'}}>Globaaal Network Brrrooo is HERE!</h1>;
