af94f96e8c977d0ba001a9addb26d82c52023a81
tigrankhh <khachtik2606@gmail.com>
2026-02-19 00:43:51 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,4 @@
 export const runtime = 'edge';
-
 export default function Home() {
-  return (
-    <div style={{ backgroundColor: '#000', color: '#fff', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <h1>Globaaal Network is LIVE, Brrrooo! 🚀</h1>
-    </div>
-  );
+  return <h1 style={{color: 'white', padding: '100px'}}>404 IS DEAD! GLOBAL NETWORK LIVE!</h1>;
 }
